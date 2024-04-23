c5f5fee309d708131d45fd8445787d713890268f
BFish <95540734+bfish1996@users.noreply.github.com>
2024-04-23 14:54:11 +0000

    copy.c

--- .ccls-cache/@home@runner@Training-and-Learning-C-v2/main.c
+++ .ccls-cache/@home@runner@Training-and-Learning-C-v2/main.c
@@ -7,26 +7,17 @@
 int main(void)
 {
   char* s = get_string("S: "); 
-  if (s == NULL)
-  {
-    return 1;
-  }
-
   char* t = malloc(strlen((s) + 1));
 
-  if (t == NULL)
-  {
-    return 1;
+  strcpy(t, s);
+
+  if (strlen(t) > 0)
+  {t[0] = toupper(t[0]);
   }
 
-  for (int i = 0, n = strlen(s); i <= n; i++)
-    {
-      t[i] = s[i];
-    }
-    if (strlen(t) > 0)
-    {
-    t[0] = toupper(t[0]);
-    }
   printf("%s\n", s);
   printf("%s\n", t);
+
+  free(t);
+  return 0;
 }
