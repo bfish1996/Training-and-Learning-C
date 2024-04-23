70d7e96edada544f1d10ad494797ca3c541eff5f
BFish <95540734+bfish1996@users.noreply.github.com>
2024-04-23 14:12:41 +0000

    compare.c

--- .ccls-cache/@home@runner@Training-and-Learning-C-v2/main.c
+++ .ccls-cache/@home@runner@Training-and-Learning-C-v2/main.c
@@ -7,12 +7,7 @@ int main(void)
   char* j = get_string("What's J?:  ");
   char* k = get_string("What's K?:  ");
 
-  if (strcmp(j, k) == 0)
-  {
-    printf("J is equal to K\n");
-  }
-  else
-  {
-    printf("Nope\n");
-  }
+  printf("%p\n", j);
+  printf("%p\n", k);
+
 }
