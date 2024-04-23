7cb361c6a84bfaf9119b3b1073c07f60ba2b9da2
BFish <95540734+bfish1996@users.noreply.github.com>
2024-04-23 13:53:59 +0000

    v2

--- .ccls-cache/@home@runner@Training-and-Learning-C-v2/main.c
+++ .ccls-cache/@home@runner@Training-and-Learning-C-v2/main.c
@@ -1,21 +1,19 @@
-#include <cs50.h>
 #include <stdio.h>
+#include <cs50.h>
 #include <string.h>
+
+void draw(int n);
+
+typedef char* Yay;
 
 int main(void)
 {
-  string names[] = {"Alice", "Bob", "Charlie", "David", "Eve"};
-  string numbers[] = {"555-1234", "555-4321", "555-5555", "555-0000", "555-1111"};
-
-  string name = get_string("Name: ");
-  for (int i = 0; i < 5; i++)
-    {
-      if (strcmp(names[i], name) == 0)
-      {
-        printf("Found %s\n", numbers[i]);
-        return 0;
-      }
-    }
-  printf("Not found\n");
-  return 1;
+  Yay s = "HI!";
+  printf("%s\n", s);
+  printf("%c", s[0]);
+  printf("%c", s[1]);
+  printf("%c\n", s[2]);
+  printf("%c\n", *s);
+  printf("%c\n", *(s+1));
+  printf("%p\n", &s[2]);
 }
