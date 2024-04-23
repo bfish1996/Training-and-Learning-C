286801dcf28f0f5a8e8dbfe89a9dd3e556d2b597
BFish <95540734+bfish1996@users.noreply.github.com>
2024-04-23 14:46:35 +0000

    copy/c

--- .ccls-cache/@home@runner@Training-and-Learning-C-v2/main.c
+++ .ccls-cache/@home@runner@Training-and-Learning-C-v2/main.c
@@ -1,13 +1,32 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 int main(void)
 {
-  char* j = get_string("What's J?:  ");
-  char* k = get_string("What's K?:  ");
+  char* s = get_string("S: "); 
+  if (s == NULL)
+  {
+    return 1;
+  }
 
-  printf("%p\n", j);
-  printf("%p\n", k);
+  char* t = malloc(strlen((s) + 1));
 
+  if (t == NULL)
+  {
+    return 1;
+  }
+
+  for (int i = 0, n = strlen(s); i <= n; i++)
+    {
+      t[i] = s[i];
+    }
+    if (strlen(t) > 0)
+    {
+    t[0] = toupper(t[0]);
+    }
+  printf("%s\n", s);
+  printf("%s\n", t);
 }
